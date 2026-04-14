88639a5c69641841629240e66deac2d7a7d35517
sreedev <xreedev@gmail.com>
2026-04-14 09:05:44 +0530

    changes

--- sqllite_service.py
+++ sqllite_service.py
@@ -1,26 +1,33 @@
 import sqlite3
 
-conn = sqlite3.connect("papers.db") 
-conn.row_factory = sqlite3.Row
-cursor = conn.cursor()
+class SQLiteService:
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS documents (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    content TEXT,
-    page INTEGER,
-    heading TEXT
-)
-""")
+    def __init__(self, db_name="database.db"):
+        conn = sqlite3.connect(db_name)
+        conn.row_factory = sqlite3.Row
+        self.cursor = conn.cursor()
+        self.conn = conn
 
-def store_sections_in_sqlite(sections):
-    cursor.executemany(
-    "INSERT INTO documents (content, page, heading) VALUES (?, ?, ?)",
-    sections)
-    conn.commit()
-    print("Sections stored in SQLite")
+    def create_table(self, table_name):
+        self.cursor.execute(f"""
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            content TEXT,
+            page INTEGER,
+            heading TEXT
+        )
+        """)
+        self.conn.commit()
+        print(f"Table '{table_name}' created in SQLite database.")
 
-def get_sections_from_sqlite():
-    cursor.execute("SELECT id, content, heading, page FROM documents")
-    results = cursor.fetchall()
-    return [dict(row) for row in results]
+    def store_sections_in_sqlite(self, sections):
+        self.cursor.executemany(
+        "INSERT INTO documents (content, page, heading) VALUES (?, ?, ?)",
+        sections)
+        self.conn.commit()
+        print("Sections stored in SQLite")
+        
+    def get_sections_from_sqlite(self):
+        self.cursor.execute("SELECT id, content, heading, page FROM documents")
+        results = self.cursor.fetchall()
+        return [dict(row) for row in results]
--- summarize_agent.py
+++ summarize_agent.py
@@ -5,10 +5,14 @@ import os
 load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
+class GeminiAgent:
 
-def summarize_scientific_paper_to_markdown(paper):
-    prompt = "Summarize this scientific paper in proper format"
-    response = client.models.generate_content(
-        model="gemini-3-flash-preview",
-        contents = prompt + paper)
-    return response.text
+    def __init__(self, system_prompt):
+        self.system_prompt = system_prompt
+
+    def summarize_scientific_paper_to_markdown(self, paper):
+        prompt = "Summarize this scientific paper in proper format"
+        response = client.models.generate_content(
+            model="gemini-3-flash-preview",
+            contents = self.system_prompt + prompt + paper)
+        return response.text
